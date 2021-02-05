f0cb8d71795fabff3a06ee6d2ab3c37368655954
Jan Gevaert <jan.gevaert@gmail.com>
2021-02-05 14:49:12 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/MethodsWithExceptions.java
@@ -0,0 +1,50 @@
+package edu.cnm.deepdive;
+
+class NoMoreMeatException extends Exception {
+
+}
+
+class NoMorePlantsException extends RuntimeException {}
+
+interface Omnivore {
+  void eatMeat(int amount) throws NoMoreMeatException;
+  void eatPlants(int amount);
+
+}
+
+class Bear implements Omnivore {
+
+  @Override
+  public void eatMeat(int amount) throws NoMoreMeatException {
+      if (amount <= 0) {
+        throw new NoMoreMeatException();
+      }
+    System.out.println("Eating Meat.");
+  }
+
+  @Override
+  public void eatPlants(int amount) throws NoMorePlantsException {
+      if (amount <= 0) {
+        throw new NoMorePlantsException();
+      }
+  }
+}
+
+public class MethodsWithExceptions {
+
+  public static void main(String[] args) {
+      Bear bear = new Bear();
+      try {
+        bear.eatMeat(2);
+        bear.eatPlants(-2);
+      } catch(NoMoreMeatException e) {
+        e.printStackTrace();
+        System.out.println("Bear goes hungry");
+      } finally {
+        System.out.println("finally");
+      }
+
+
+  }
+
+}
